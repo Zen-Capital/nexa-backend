c3407baf3678141ebc2b520b63de81789e577b3c
Marcos <marcos.lprado@sempreceub.com>
2024-11-10 13:56:49 -0300

    feat: implements function that connects to mongodb cluster

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,9 @@
 package main
 
-func main() {}
+import (
+	"github.com/Zen-Capital/nexa-backend/database"
+)
+
+func main() {
+	database.ConnectDatabase()
+}
---
+++ database/database.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDatabase() {
+	err := godotenv.Load("mongo.env")
+	if err != nil {
+		log.Fatal("Erro ao carregar .env")
+	}
+
+	mongoURI := os.Getenv("MONGO_URI")
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("Falha ao conectar ao MongoDB:", err)
+	}
+
+	DB = client.Database("Nexa")
+	log.Println("Conectado ao MongoDB com segurança")
+}
