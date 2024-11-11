a7a39c16d6f968b0bb48bd00cd76024d9d88506d
Marcos <marcos.lprado@sempreceub.com>
2024-11-11 19:42:03 -0300

    feat: implements fiber

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"github.com/Zen-Capital/nexa-backend/database"
+	"github.com/Zen-Capital/nexa-backend/routes"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	database.ConnectDatabase()
+	app := fiber.New()
+
+	routes.SetupRoutes(app)
+
+	app.Listen(":3000")
 }
