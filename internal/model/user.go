c24083c15e92aa70ca9a4826d7498d4e3586b6ca
Marcos <marcos.lprado@sempreceub.com>
2024-11-13 15:45:36 -0300

    feat: adds date_joined atribute to user

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,13 +1,18 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Name     string             `json:"name" bson:"name"`
-	Username string             `json:"username" bson:"username"`
-	Password string             `json:"password" bson:"password"`
-	Email    string             `json:"email" bson:"email"`
-	Account  Account            `json:"account" bson:"account"`
-	IsActive bool               `json:"is_active" bson:"is_active"`
+	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name       string             `json:"name" bson:"name"`
+	Username   string             `json:"username" bson:"username"`
+	Password   string             `json:"password" bson:"password"`
+	Email      string             `json:"email" bson:"email"`
+	Account    Account            `json:"account" bson:"account"`
+	IsActive   bool               `json:"is_active" bson:"is_active"`
+	DateJoined time.Time          `json:"date_joined" bson:"date_joined"`
 }
