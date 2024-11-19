d1fb77fd751cb2340a2e614df2a60cd0b308eb43
Marcos <marcos.lprado@sempreceub.com>
2024-11-19 16:24:25 -0300

    fix: fixes writing issues

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -19,19 +19,19 @@ func createUser(c *fiber.Ctx) error {
 	var input UserInput
 	if err := c.BodyParser(&input); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"erro": "Input Inválido",
+			"error": "Input Inválido",
 		})
 	}
 
 	if len(input.Password) < 6 {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"erro": "Senha precisa ter no mínimo 6 caracteres",
+			"error": "Senha precisa ter no mínimo 6 caracteres",
 		})
 	}
 
 	if input.Username == "" || input.Email == "" {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"erro": "Todos os campos precisam ser prenchidos",
+			"error": "Todos os campos precisam ser prenchidos",
 		})
 	}
 
@@ -39,7 +39,7 @@ func createUser(c *fiber.Ctx) error {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"erro": "Erro ao encriptar senha",
+			"error": "Erro ao encriptar senha",
 		})
 	}
 
@@ -56,10 +56,7 @@ func createUser(c *fiber.Ctx) error {
 
 	if err := service.SaveUser(user); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"erro": "Erro ao salvar usuário",
+			"error": "Erro ao salvar usuário",
 		})
 	}
-
-	// Geração de Token JWT //
-
 }
